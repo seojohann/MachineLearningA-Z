d36b6f52787596c42bdeb4053db9ed6c2f1fe416
seojohann <seojohann@gmail.com>
2018-07-10 14:42:41 -0700

    uploaded polynomial regression & regression template

--- Part 2 - Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -39,3 +39,20 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# optimize model using backward elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+SL = 0.05
+
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+# remove feature with highest P-value -> [2]x2 (.990) since it is greater than SL
+# remove next feature with highest P-value -> [1]x1 (.940) > SL
+# remove next feature with highest P-value -> [4]x2 (.602) > SL
+# remove next feature with highest P-value -> [5]x2 (.06) > SL
+X_opt = X[:, [0, 3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
